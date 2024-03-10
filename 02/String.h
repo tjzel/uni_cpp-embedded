8c4b6e69d91c356e83e0720c0e02285933d422c9
Tomasz Żelawski <tzelawski@gmail.com>
2024-03-10 15:29:38 +0100

    finish

--- 02/String.h
+++ 02/String.h
@@ -1,23 +1,20 @@
 #include "MyString.h"
 #include <algorithm>
 #include <iostream>
-using namespace std;
+#include <memory>
 
 class String {
   using string = emcpp::MyString;
   //   using string = std::string;
 
-  /// Store a pointer to dynamically allocated string!
-
   std::shared_ptr<string> str_ptr;
 
 public:
-  String() : str_ptr(make_shared<string>()){}; /// creates an empty string
+  String() : str_ptr(std::make_shared<string>()){};
 
-  String(const char *str)
-      : str_ptr(make_shared<string>(str)){}; /// copy C-string
+  String(const char *str) : str_ptr(std::make_shared<string>(str)){};
 
-  String(const String &str) : str_ptr(str.str_ptr){}; /// no copy
+  String(const String &str) : str_ptr(str.str_ptr){};
 
   String operator=(const String &s) {
     if (this == &s) {
@@ -25,20 +22,17 @@ public:
     }
     str_ptr = s.str_ptr;
     return *this;
-  }; /// no copy
+  };
 
-  /// makes a copy of a string if it has more than one reference.
   void set(int index, char ch) {
     if (str_ptr.use_count() > 1) {
       str_ptr = std::make_shared<string>(*str_ptr);
     }
-    str_ptr->assign(index, ch);
+    (*str_ptr)[index] = ch;
   }
 
-  /// no copy
   char get(int index) const { return str_ptr->at(index); }
 
-  /// creates a new string only if both strings are non empty
   friend String operator+(String a, String b) {
     if (a.str_ptr->empty()) {
       return b;
