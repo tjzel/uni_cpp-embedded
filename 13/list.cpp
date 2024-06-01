5d505b2c39197e2cb818c27edd2f3b381c3b7453
Tomasz Żelawski <tzelawski@gmail.com>
2024-06-01 11:44:52 +0200

    13 done

--- 13/list.cpp
+++ 13/list.cpp
@@ -4,42 +4,48 @@
 #include <iomanip>
 #include <iostream>
 
-template <int N, int... Rest> struct IntegerList;
+template <int Head, int... Rest> struct IntegerList;
 
-template <int N, typename IIntegerList> struct getInt;
+template <int Index, typename List> struct getInt;
 
-template <int N, int... Rest> struct IntegerList {
+template <typename List> struct Max;
+
+template <int Head, int... Rest> struct IntegerList {
   typedef IntegerList<Rest...> RestList;
-  static constexpr int value = N;
+  static constexpr int value = Head;
 
   static constexpr int get(int index) {
     if (index == 0) {
-      return N;
-    } else {
+      return Head;
+    }
+    else {
       return RestList::get(index - 1);
     }
   }
+
+  static constexpr int max() {
+    return Max<IntegerList<Head, Rest...>>::value;
+  }
 };
 
-template <int N> struct IntegerList<N> {
-  static constexpr int value = N;
+template <int Head> struct IntegerList<Head> {
+  static constexpr int value = Head;
 
-  static constexpr int get(int index) { return N; }
+  static constexpr int get(int index) { return Head; }
+
+  static constexpr int max() { return Head; }
 };
 
-template <int N, typename IIntegerList> struct getInt {
-  static constexpr int value =
-      getInt<N - 1, typename IIntegerList::RestList>::value;
+
+template <int Index, int Head, int... Rest>
+struct getInt<Index, IntegerList<Head, Rest...>> {
+  static constexpr int value = Index == 0 ? Head : IntegerList<Rest...>::get(Index - 1);
 };
 
-template <typename IIntegerList> struct getInt<0, IIntegerList> {
-  static constexpr int value = IIntegerList::value;
-};
-
-template <int N, int... Rest>
-std::ostream &operator<<(std::ostream &os,
-                         const IntegerList<N, Rest...> &list) {
-  os << N;
+template <int Head, int... Rest>
+std::ostream& operator<<(std::ostream& os,
+  const IntegerList<Head, Rest...>& list) {
+  os << Head;
   if constexpr (sizeof...(Rest) > 0) {
     os << ", ";
     return operator<<(os, IntegerList<Rest...>());
@@ -47,7 +53,32 @@ std::ostream &operator<<(std::ostream &os,
   return os;
 }
 
-template <int IIntegerList> struct IsSorted {};
+template <typename List> struct IsSorted;
+
+template <int Head, int... Rest> struct IsSorted<IntegerList<Head, Rest...>> {
+  static constexpr bool value = (Head <= IntegerList<Rest...>::value) && IsSorted<IntegerList<Rest...>>::value;
+};
+
+template <int Head> struct IsSorted<IntegerList<Head>> {
+  static constexpr bool value = true;
+};
+
+template <int Head, int... Rest> struct Max<IntegerList<Head, Rest...>> {
+  static constexpr int value = Head > Max<IntegerList<Rest...>>::value ? Head : Max<IntegerList<Rest...>>::value;
+};
+
+template <int Head> struct Max<IntegerList<Head>> {
+  static constexpr int value = Head;
+};
+
+template <typename LeftList, typename RightList> struct Join;
+
+template <int HeadLeft, int... RestLeft, int HeadRight, int... RestRight>
+struct Join<IntegerList<HeadLeft, RestLeft...>, IntegerList<HeadRight, RestRight...>> {
+  using type = IntegerList<HeadLeft, RestLeft..., HeadRight, RestRight...>;
+};
+
+
 
 using namespace std;
 int main() {
@@ -64,14 +95,14 @@ int main() {
   static_assert(listB::get(1) == 4);
 
   cout << boolalpha;
-  // cout << "Is A sorted? " << IsSorted<listA>::value << endl;
-  // cout << "Is B sorted? " << IsSorted<listB>::value << endl;
+  cout << "Is A sorted? " << IsSorted<listA>::value << endl;
+  cout << "Is B sorted? " << IsSorted<listB>::value << endl;
 
-  // using listC = Join<listA, listB>::type;
+  using listC = Join<listA, listB>::type;
 
-  // cout << "List C : " << listC() << endl;
-  // cout << Max<listC>::value << endl;
-  // cout << listC::max() << endl;
-  // static_assert(listC::max() == 9);
-  // static_assert(listA::max() == 5);
+  cout << "List C : " << listC() << endl;
+  cout << Max<listC>::value << endl;
+  cout << listC::max() << endl;
+  static_assert(listC::max() == 9);
+  static_assert(listA::max() == 5);
 }
